4fd1c89b6c8eb1621eb22288f770fd79a266dd4c
Suciu Marius <110302280+sumarius56@users.noreply.github.com>
2022-10-31 18:38:24 +0200

    added github provider, google is fucking idiot and not working...

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,12 +3,17 @@ import GoogleProvider from "next-auth/providers/google";
 import { db } from "../../../firebase";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 import * as firestoreFunctions from "firebase/firestore";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   secret: process.env.JWT_SECRET,
