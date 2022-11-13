db8f27bf537f0a5bf479acf35dc76582b7a06c4e
Suciu Marius <110302280+sumarius56@users.noreply.github.com>
2022-11-13 12:29:28 +0200

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -69,66 +69,66 @@ function search() {
   return (
     <>
       <Header />
-    <div className="flex flex-col overflow-x-hidden items-center justify-center">
-      <div className="flex mx-auto  mt-20">
-        <div className="flex space-x-5 items-center rounded-lg  ">
-          <input
-            className="text-[#031126] py-1 px-2 rounded-lg min-w-[200px] md:min-w-[300px]"
-            type="text"
-            label="Search"
-            onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Search your favourite movies..."
-          />
-          <button
-            className="border py-1 px-2.5 rounded-lg font-bold"
-            onClick={() => {
-              fetchSearch()
-              setCurrentPage(1)
-            }}
-          >
-            Search
-          </button>
-        </div>
-      </div>
-      <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto mt-[100px]">
-        <h2 className="font-semibold"></h2>
-
-        <div
-          id="container"
-          className="grid sm:grid-cols-2 fuckingShit  lg:grid-cols-3 items-center overflow-x-hidden  mx-auto space-y-3  space-x-6  scrollbar-thin  scrollbar-thumb-[#0c0421] scrollbar-track-[#313e54] p-2 pb-4 -m-3 "
-        >
-          {currentMovies?.map((result) => (
-            <MovieThumbnailSearch
-              key={result?.id + Math.random() * Math.random()}
-              result={result}
+      <div className="flex flex-col overflow-x-hidden items-center justify-center">
+        <div className="flex mx-auto  mt-20">
+          <div className="flex space-x-5 items-center rounded-lg  ">
+            <input
+              className="text-[#031126] py-1 px-2 rounded-lg min-w-[200px] md:min-w-[300px]"
+              type="text"
+              label="Search"
+              onChange={(e) => setSearchText(e.target.value)}
+              placeholder="Search your favourite movies..."
             />
-          ))}
+            <button
+              className="border py-1 px-2.5 rounded-lg font-bold"
+              onClick={() => {
+                fetchSearch();
+                setCurrentPage(1);
+              }}
+            >
+              Search
+            </button>
+          </div>
         </div>
+        <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto mt-[100px]">
+          <h2 className="font-semibold"></h2>
 
-        <div className="flex space-x-3 mx-auto items-center">
-          <button
-            className="hover:scale-115 hover:animate:pulse"
-            onClick={() =>
-              setCurrentPage((prevPage) =>
-                prevPage > 1 ? prevPage - 1 : prevPage
-              )
-            }
+          <div
+            id="container"
+            className="grid sm:grid-cols-2 fuckingShit  lg:grid-cols-3 items-center overflow-x-hidden  mx-auto space-y-3  space-x-6  scrollbar-thin  scrollbar-thumb-[#0c0421] scrollbar-track-[#313e54] p-2 pb-4 -m-3 "
           >
-            <ChevronDoubleLeftIcon className="w-10 hover:text-[#313e54] " />
-          </button>
-          <p className="font-bold border rounded-full px-3 py-1 flex items-center justify-center">
-            {" "}
-            {currentPage}
-          </p>
-          <button
-            className="hover:scale-115 hover:animate:pulse"
-            onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
-          >
-            <ChevronDoubleRightIcon className="w-10 hover:text-[#313e54] " />
-          </button>
+            {currentMovies?.map((result) => (
+              <MovieThumbnailSearch
+                key={result?.id + Math.random() * Math.random()}
+                result={result}
+              />
+            ))}
+          </div>
+
+          <div className="flex space-x-3 mx-auto items-center">
+            <button
+              className="hover:scale-115 hover:animate:pulse"
+              onClick={() =>
+                setCurrentPage((prevPage) =>
+                  prevPage > 1 ? prevPage - 1 : prevPage
+                )
+              }
+            >
+              <ChevronDoubleLeftIcon className="w-10 hover:text-[#313e54] " />
+            </button>
+            <p className="font-bold border rounded-full px-3 py-1 flex items-center justify-center">
+              {" "}
+              {currentPage}
+            </p>
+            <button
+              className="hover:scale-115 hover:animate:pulse"
+              onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
+            >
+              <ChevronDoubleRightIcon className="w-10 hover:text-[#313e54] " />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
